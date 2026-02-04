275594b4dc13ee72bbae004580dc83d0a23f9c07
Shivani.Talwad <shivani.talwad@mayuraconsultancy.com>
2026-02-04 18:36:17 +0530

    added the project

---
+++ employeeServer/src/main/java/com/rohnbhatia/employeeServer/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.rohnbhatia.employeeServer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping(value = "/health")
+    public String getHealth(){
+        return "UP!";
+    }
+
+}
